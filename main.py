f236c397ef802b192e308ba0e889fe2d8645be99
Python_Elidas <pyro.elidas@gmail.com>
2021-12-07 13:37:05 +0100

    Main Window finished, may suffer some small changes

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ Version: 0.0.0
 # __LYBRARIES__ #
 from tkinter import *
 from tkinter import filedialog, messagebox
+from tkinter.ttk import Progressbar
 import os as file
+import openpyxl as xls
 from siguiente import Excel
 
 
@@ -23,8 +25,40 @@ class Main_Window(Tk):
 
         self.title('Contabilidad Zotz')
         self.iconbitmap('files/logo.ico')
-        self.geometry('500x200')
+        self.geometry('600x250')
         self.resizable(FALSE, FALSE)
+        
+    def run(self):
+        dir = self._frame.folder.get()
+        bills = file.listdir(dir)
+        excel = self._frame.excel.get()
+        # Comprobamos que facturas han sido pasadas para no repetir
+        process = list()
+        for bil in bills:
+            nme = bil.split('.')[0].replace('-MAKRO', '')
+            if '.pdf' in bil and not nme in self._frame.ws_nms:
+                process.append(bil)
+        self.switch_frames(Info)
+        Label(self._frame, text='Procesando Archivos...')\
+            .grid(row=3)
+        try:
+            for bil in process:
+                pdf = dir.replace('C:', '//') + '/' + bil
+                Label(self._frame, text=f'{pdf}')
+                Excel(excel, pdf)
+                process.pop(process.index(bil))
+        except PermissionError:
+            messagebox.showerror(
+                message="Cierra el fichero Excel y vuelve a intentarlo",
+                title="Permission Error"
+            )
+            self.run()
+        
+    def switch_frames(self, frame):
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = frame(self)
+        self._frame.pack(fill=NONE, expand=1)
 
 
 class Main_Frame(Frame):
@@ -37,17 +71,17 @@ class Main_Frame(Frame):
             .grid(row=0, column=0, sticky=W)
         self.folder = Entry(self, width=30)
         self.folder.grid(row=1, column=0, sticky=W)
-        self.f_b = Button(self, text='···', command=self.select_folder)
+        self.f_b = Button(self, text='···', width=3, command=self.select_folder)
         self.f_b.grid(row=1, column=1)
 
         Label(self, text='Selecciona el Excel')\
             .grid(row=0, column=2, sticky=W, padx=10)
         self.excel = Entry(self, width=35)
         self.excel.grid(row=1, column=2, sticky=W, padx=10)
-        self.f_e = Button(self, text='···', command=self.select_excel)
+        self.f_e = Button(self, text='···', width=3, command=self.select_excel)
         self.f_e.grid(row=1, column=3, sticky=E)
 
-        self.exe = Button(self, text='Run', command=self.run)
+        self.exe = Button(self, text='Run', command=self._master.run)
         self.exe.grid(row=2, column=3, sticky=E, pady=10)
 
     def select_excel(self):
@@ -55,6 +89,7 @@ class Main_Frame(Frame):
             init = '/'
         else:
             init = self.folder.get()
+            init = '/'.join(init.split('/')[:-1])
         xcel = filedialog.askopenfilename(
             initialdir=init,
             title='Selecciona el Libro de Excel',
@@ -62,25 +97,13 @@ class Main_Frame(Frame):
         )
         self.excel.delete(0, END)
         self.excel.insert(0, xcel)
+        wb = xls.load_workbook(self.excel.get(), read_only=True)
+        self.ws_nms = list(wb.sheetnames)
 
     def select_folder(self):
         dir = filedialog.askdirectory()
         self.folder.delete(0, END)
         self.folder.insert(0, dir)
-
-    def run(self):
-        dir = self.folder.get()
-        bills = file.listdir(dir)
-        try:
-            for bil in bills:
-                pdf = dir.replace('C:', '//') + '/' + bil
-                Excel(self.excel.get(), pdf)
-        except PermissionError:
-            messagebox.showerror(
-                message="Cierra el fichero Excel y vuelve a intentarlo",
-                title="Permission Error"
-            )
-            self.run()
 
 
 def run():
@@ -98,5 +121,6 @@ if __name__ == '__main__':
 
 # __BIBLIOGRAPHY__ #
 '''
+    · ProgressBar: https://recursospython.com/guias-y-manuales/barra-de-progreso-progressbar-tcltk-tkinter/
 
 '''
